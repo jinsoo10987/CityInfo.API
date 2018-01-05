f935e6fdfadf969bdf6fdf48b64b36600d5c1f19
Jinsoo Kim <jinsoo.kim.kr@gmail.com>
2018-01-05 00:05:14 -0600

    Support XML output format

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API
@@ -10,7 +11,10 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+
             // This is for making returned properties names the same as defiend in model class.
             // Otherwise, the name automatically converted to camel case.
             //.AddJsonOptions(o =>
